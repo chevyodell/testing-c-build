5bc8a1b8d1ec08a14c33b18ca76befef7d81698d
chevyodell <chevy.odell@gmail.com>
2021-06-15 14:34:33 -0600

    Create main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+//Just boilerplate c code to play with
+
+#include <stdio.h>
+#include "func.h"
+
+int main(void) {
+  int a = 4;
+  int b = 6;
+  
+  int c = func(a,b);
+
+  printf("Result is: %d\n", c);
+}
